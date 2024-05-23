9b6c9661ba56ad9c0f30b178d0ad2ff1b2633e68
mohammodjuwelislam <mohammodjuwelislam@gmail.com>
2024-05-23 15:13:52 +0600

    hismile home page full created

--- js/main.js
+++ js/main.js
@@ -15,22 +15,25 @@ window.addEventListener('scroll', () => {
 
 
 // mobile menu 
-/* const menu_bar = document.getElementById('menu_bar_icon');
+const menu_bar = document.getElementById('menu_bar_icon');
 const menu_bar_close = document.getElementById('menu_bar_close_icon');
-const menu_bar_overlay = document.getElementById('menu_bar_close_overlay');
+
+const mobile_menu_overlay = document.getElementById('mobile_menu_overlay');
 const mobile_menu = document.getElementById('mobile_menu');
 menu_bar.addEventListener('click', () => {
     mobile_menu.classList.add('active');
-    mobile_menu.classList.add('active');
+    mobile_menu_overlay.classList.add('active')
 });
+
 menu_bar_close.addEventListener('click', () => {
     mobile_menu.classList.remove('active');
-    mobile_menu.classList.remove('active');
+    mobile_menu_overlay.classList.remove('active');
 });
-mobile_menu.addEventListener('click', () => {
+
+mobile_menu_overlay.addEventListener('click', () => {
     mobile_menu.classList.remove('active');
-    mobile_menu.classList.remove('active');
-}); */
+    mobile_menu_overlay.classList.remove('active');
+})
 
 
 
